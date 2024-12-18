c2559c57876f7d256cc8bbea379830f0cd96281f
SebastianCaicedo17 <sebastianquesadacaicedo@gmail.com>
2024-12-18 17:24:18 +0100

    main_game avec la bonne condition de fin de la boucle for

--- main_game.py
+++ main_game.py
@@ -13,7 +13,7 @@ def main_game():
     player_list = player_turn.player_turn()
     verification.verification(game_list, player_list)
     for game_turn in range (10):
-        if game_turn == 10: print("Vous avez perdu")
+        if game_turn == 9: print("Vous avez perdu")
         print("Tour n°", game_turn+1)
         verification.verification(game_list, player_list)
         player_turn.player_turn()
