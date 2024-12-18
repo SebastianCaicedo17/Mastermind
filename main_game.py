b8a345ef7c43b32da00e6f7cba1c6f7023aaf08d
SebastianCaicedo17 <sebastianquesadacaicedo@gmail.com>
2024-12-18 17:35:31 +0100

    Condition break lors de fin de boucle

--- main_game.py
+++ main_game.py
@@ -13,10 +13,12 @@ def main_game():
     player_list = player_turn.player_turn()
     verification.verification(game_list, player_list)
     for game_turn in range (10):
-        if game_turn == 9: print("Vous avez perdu")
+        if game_turn == 1: 
+            print("Vous avez perdu")    
+            break
         print("Tour n°", game_turn+1)
         verification.verification(game_list, player_list)
-        player_turn.player_turn()
+        player_list = player_turn.player_turn()
 
 
 main_game ()
